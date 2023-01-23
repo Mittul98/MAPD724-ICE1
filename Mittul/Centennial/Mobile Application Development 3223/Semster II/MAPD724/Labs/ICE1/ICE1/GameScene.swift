4efb5cfe97666fac948ff079ff435b95492b7b9e
Mittul Trivedi <113215495+Mittul98@users.noreply.github.com>
2023-01-22 19:45:41 -0500

    added Project Assets

--- Mittul/Centennial/Mobile Application Development 3223/Semster II/MAPD724/Labs/ICE1/ICE1/GameScene.swift
+++ Mittul/Centennial/Mobile Application Development 3223/Semster II/MAPD724/Labs/ICE1/ICE1/GameScene.swift
@@ -12,21 +12,21 @@ class GameScene: SKScene
 {
     override func sceneDidLoad()
     {
-        
+        print("Game Scene Loaded")
     }
-    
-    
     func touchDown(atPoint pos : CGPoint)
     {
+        print("Touch Down")
     }
     
     func touchMoved(toPoint pos : CGPoint)
     {
-        
+        print("Touch Move")
     }
     
     func touchUp(atPoint pos : CGPoint)
     {
+        print("Touch Up")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
